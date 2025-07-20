16a7c1599e9616f475709949cefcb944567f7fc8
malken21 <77374813+malken21@users.noreply.github.com>
2025-07-20 21:25:32 +0900

    : to _

--- src/secret.nim
+++ src/secret.nim
@@ -5,6 +5,6 @@ import std/strformat
 proc getEnvMap*(): seq[(string, string)] =
   var envMap: seq[(string, string)] = @[]
   for key, value in envPairs():
-    if key.match(re".+:.+"):
+    if key.match(re".+_.+"):
       envMap.add((fmt"[[{key}]]", value))
   return envMap
